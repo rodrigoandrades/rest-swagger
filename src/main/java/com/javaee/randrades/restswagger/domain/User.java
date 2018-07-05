e789d710a12ae30e0993e8d339106e1adbebf6a2
Rodrigo Andrades <rodrigomail2007@gmail.com>
2018-07-05 19:18:31 -0300

    conclusao

---
+++ src/main/java/com/javaee/randrades/restswagger/domain/User.java
@@ -0,0 +1,50 @@
+package com.javaee.randrades.restswagger.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+	private Long id;
+	
+	private String name;
+	
+	private String password;
+	
+	private Boolean active;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+	
+}
